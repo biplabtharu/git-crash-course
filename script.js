2b04acf60a0e30fa6eee4e859802c12e39a7d2f7
Biplab Tharu <kktbiplab010@gmail.com>
2024-03-06 13:37:55 +0545

    added feature 1 and 2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function script() {
   console.log(`script file added`);
 }
+
+script();
+
+function features() {
+  console.log(`This is feature 1`);
+  console.log(`This is feature 2`);
+}
+features();
